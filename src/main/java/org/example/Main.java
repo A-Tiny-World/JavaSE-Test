fc2a9fd438afb8671e27348ab2260233dd48ee70
c <fjczh0@163.com>
2023-06-17 10:27:30 +0800

    变更1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        int x=1,y=2,z=3;
         List<String> strList=new ArrayList<>();
         strList.add("test1");
         strList.add("test2");
